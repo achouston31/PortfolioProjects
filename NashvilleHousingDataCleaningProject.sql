8eb61664c9421536b5ecefdeb026769124150080
achouston31 <141458765+achouston31@users.noreply.github.com>
2023-08-06 20:46:56 -0500

    Add files via upload

---
+++ NashvilleHousingDataCleaningProject.sql
@@ -0,0 +1,189 @@
+/***********************************************************************************************
+
+Object Name:
+
+Object Type: SQL Script
+
+Description: Cleaning Data from Nashville Housing Data
+
+Author: Andrew Houston
+
+Create Date: 08.06.2023
+
+Change History
+
+------------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------------
+
+Follow-up notes:
+
+Some steps are just to show how to perform a function even if best practice is to not perform those tasks. 
+
+************************************************************************************************/
+
+SELECT *
+FROM NashvilleHousing
+
+
+-- Standardizing the date formatting. 
+
+Select SaleDate, CONVERT(Date,SaleDate)
+From NashvilleHousing
+
+
+Update NashvilleHousing
+SET SaleDate = CONVERT(Date,SaleDate)
+
+-- The table did not update properly. I added a new column and converted to proper date format. 
+
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+SELECT *
+FROM NashvilleHousing
+
+
+
+-- Populate Property Address
+
+SELECT *
+FROM NashvilleHousing
+--WHERE PropertyAddress IS NULL
+ORDER BY ParcelID
+
+--Doing a Self Join to fill NULL values in Property address by looking at Parcel ID and matching them together. 
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM NashvilleHousing a
+JOIN NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM NashvilleHousing a
+JOIN NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+
+
+-- Seperating Address into Seperate Columns (ddress, City, State)
+
+SELECT PropertyAddress
+FROM NashvilleHousing
+--ORDER BY ParcelID
+
+--We are using "," as a delimiter to seperate the Street Address from the City
+SELECT 
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address,
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) AS City
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+
+SELECT *
+FROM NashvilleHousing
+
+
+--Updating Owner Address
+
+SELECT OwnerAddress
+From NashvilleHousing
+
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),3) AS Address,
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),2)AS City, 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),1) AS State
+From NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'),3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'),2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'),1)
+
+SELECT *
+FROM NashvilleHousing
+
+
+--Changing "Y" and "N" to "Yes" and "No" in "Sold as Vacant" Column
+
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
+FROM NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'Y' Then 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
+FROM NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' Then 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
+
+
+--Remove Duplicate Data (Not typically done with raw data)
+
+WITH RowNumCTE AS(
+SELECT *, 
+	ROW_NUMBER () OVER(
+	PARTITION BY ParcelID, 
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY 
+					UniqueID
+					) row_num				 
+FROM NashvilleHousing
+--ORDER BY ParcelID
+)
+SELECT *
+--DELETE *Used to Delete duplicate and then executed with SELECT to verify. 
+FROM RowNumCTE
+WHERE row_num > 1
+--ORDER BY PropertyAddress
+
+
+--Delete unused columns
+
+SELECT *
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
